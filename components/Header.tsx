467871560a9bcd767aa8063512105de6018864f5
Ergys <gysigba@gmail.com>
2023-02-20 22:56:28 +0100

    Responsive header, Hero set up

---
+++ components/Header.tsx
@@ -0,0 +1,66 @@
+import {motion} from "framer-motion";
+import React from "react";
+import {SocialIcon} from "react-social-icons";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header className="sticky top-0 flex p-5 items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://www.instagram.com/lacreativehub/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.facebook.com/lacreativehub"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          url="mailto:lacreativehub4@gmail.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get in touch
+        </p>
+      </motion.div>
+    </header>
+  );
+}
+
+export default Header;
