f7b5d6bdf567b12ef6cfa2fdf18f7ea28453dda7
Ergys <gysigba@gmail.com>
2023-02-22 20:50:11 +0100

    fixed hero section, added section buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Cursor, useTypewriter} from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {};
 
@@ -20,17 +21,34 @@ export default function Hero({}: Props) {
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-central overflow-hidden">
       <BackgroundCircles />
       <Image
-        className="relative rounded-full h-44 w-44 mx-auto object-cover"
+        className="relative rounded-full h-44 w-44 mx-auto object-cover mt-0"
         src="https://lacreativehub.com/wp-content/uploads/2022/01/Ergys-Sono-picture.jpg"
         alt="Ergys Sono, Software developer"
         width={200}
         height={200}
       />
-      <div className="z-10">
-        <h1>
+      <div className="z-10 text-center">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Design and Development
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
           <span>{text}</span>
           <Cursor cursorColor="#000" />
         </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
